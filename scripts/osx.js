72ec2328e3cdf0ebdc22fed8771b0dd2c8798e6b
Andy Hull <andyhull3@gmail.com>
2011-11-09 14:35:57 -0500

    more new stuff

---
+++ scripts/osx.js
@@ -0,0 +1,77 @@
+/*
+ * SimpleModal OSX Style Modal Dialog
+ * http://www.ericmmartin.com/projects/simplemodal/
+ * http://code.google.com/p/simplemodal/
+ *
+ * Copyright (c) 2010 Eric Martin - http://ericmmartin.com
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Revision: $Id: osx.js 238 2010-03-11 05:56:57Z emartin24 $
+ */
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("th.osx, #formsubmit").click(function (e) {
+				e.preventDefault();		 
+				$("#holder_popup").html($("#"+$(this).attr('link')).html());
+				$("#holder_popup").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 80,
+					opacity: 65, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					marker: 'd',
+					onClose: OSX.close
+				});
+				if ($(this).attr('link') == 'thankyou_popup') {
+					$('#rateForm').ajaxSubmit(); 
+					return false;
+				}
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('slow', function () {
+				$("#holder_popup", self.container).show();
+				var title = $("#osx-modal-title", self.container);
+				title.show();
+				d.container.slideDown('slow', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							200,
+							function () {
+								$("div.close", self.container).show();
+								$("#osx-modal-data", self.container).show();
+							}
+						);
+					}, 300);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				500,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
